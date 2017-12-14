ce48e83f2558d863b59761f255c99e18706fdafd
Juan Carlos Vallejo <jcvallejo@start-app.es>
2017-12-14 08:54:28 +0100

    Bottom navigation ready

--- app/src/main/java/com/urizev/birritas/MainActivity.java
+++ app/src/main/java/com/urizev/birritas/MainActivity.java
@@ -1,44 +1,46 @@
 package com.urizev.birritas;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+    @BindView(R.id.main_pager) ViewPager viewPager;
+    @BindView(R.id.main_navigation) BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = item -> {
+        int res;
+        switch (item.getItemId()) {
+            case R.id.navigation_featured:
+                res = R.string.title_featured;
+                Toast.makeText(this, res, Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.navigation_map:
+                res = R.string.title_map;
+                Toast.makeText(this, res, Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.navigation_favorites:
+                res = R.string.title_favorites;
+                Toast.makeText(this, res, Toast.LENGTH_SHORT).show();
+                return true;
+        }
+
+        return false;
+    };
 
 }
